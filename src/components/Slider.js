ebbb015990bda959df4e7d1b598f6ce154c92195
binraw <truvelotrobin@yahoo.fr>
2023-06-21 09:35:02 +0200

    avancer sur slide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Slider(props) {
+	const navigate = useNavigate();
+
 	const handleClickNext = () => {
 		props.handleClickNext();
+		handleRedirect();
 	};
 
 	const handleClickPrevious = () => {
 		props.handleClickPrevious();
+		handleRedirect();
+	};
+
+	const handleRedirect = () => {
+		navigate("/card/f72a452f");
 	};
 
 	return (
