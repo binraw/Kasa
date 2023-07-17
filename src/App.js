ef3c8253164922ae0f1385a46587d5a098d2a854
binraw <truvelotrobin@yahoo.fr>
2023-07-17 11:09:16 +0200

    fix responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./style/style.css";
 import AppRouter from "./AppRouter";
-// import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<div>
-			{/* <Header /> */}
 			<AppRouter />
 		</div>
 	);
